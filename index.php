a23035447c6362ad718fb4d0aed4ce064b6fe19c
zsxdcfv2543 <99699573+zsxdcfv2543@users.noreply.github.com>
2022-10-12 11:07:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
     
       <div class="w3-white w3-text-grey w3-card-4">
         <div class="w3-display-container">
-          <img src="../Downloads/PicsArt_09-26-09.45.20.jpg" style="width:100%" alt="Avatar">
+          <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/242877860_1576174672759179_7529231060200336129_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEhc3VpV1reuGcxrpUeBQEXafgQ-qeS3AJp-BD6p5LcArY90t2a5_y8kAm2Q_MS3x4XvhvQmm4R5Tx5uSZdya4K&_nc_ohc=GRfHFUM_3IUAX_kr8po&_nc_ht=scontent.fkkc3-1.fna&oh=00_AT_uchgRjG8E0AA261F01ylW3GJ0Gr-nBgRzki8PZMQkHw&oe=634B24A1" style="width:100%" alt="Avatar">
           <div class="w3-display-bottomleft w3-container w3-text-black">
            
           </div>
