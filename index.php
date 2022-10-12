b2363da71764bf5c5bc5f953fd84ade096e643ed
zsxdcfv2543 <99699573+zsxdcfv2543@users.noreply.github.com>
2022-10-12 11:06:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,150 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+<title>Vachira Poomsaka CV</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
 </head>
-<body>
-    <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/242877860_1576174672759179_7529231060200336129_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEhc3VpV1reuGcxrpUeBQEXafgQ-qeS3AJp-BD6p5LcArY90t2a5_y8kAm2Q_MS3x4XvhvQmm4R5Tx5uSZdya4K&_nc_ohc=GRfHFUM_3IUAX_kr8po&_nc_ht=scontent.fkkc3-1.fna&oh=00_AT_uchgRjG8E0AA261F01ylW3GJ0Gr-nBgRzki8PZMQkHw&oe=634B24A1" style="width:50%;">
-        <h1><?php echo 'Vachira Poomsaka'; ?>
-        <h1><?php echo '62010914609'; ?></h1>
+<body class="w3-aqua">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="../Downloads/PicsArt_09-26-09.45.20.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+           
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-aqua"></i>Vachira Poomsaka</p>
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-aqua"></i>Student</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-aqua"></i>Maha Sarakham, Thailand</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-aqua"></i>62010914609@msu.ac.th</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-aqua"></i>0941971793</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-aqua"></i>Skills</b></p>
+          <p>Python</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-aqua" style="width:30%">30%</div>
+          </div>
+          <p>SPSS</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-aqua" style="width:20%">
+              <div class="w3-center w3-text-black">20%</div>
+            </div>
+          </div>
+          <p>Sentimal Analysis</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-aqua" style="width:10%">10%</div>
+          </div>
+          <p>R</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-aqua" style="width:20%">20%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-aqua"></i>Languages</b></p>
+                     <p>Thai</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:90%">90%</div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:50%">50%</div>
+          </div>
+          <p>Lao</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:30%">30%</div>
+          </div>
+          <p>Japan</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:10%">10%</div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Work Experience (Nothing)</h2>    
+      </div>
+        
+               
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Business Information Technology, Mahasarakham University</b></h5>
+          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019-2022</h6>
+          <p>Studying</p>
+          <hr>
+        </div>
+      </div>
+      
+      
+       <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-book fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Academic Work (Nothing)</h2>
+
+      </div>
+      
+      
+       <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Self-Development</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>
+สุดยอดแนวคิดธุรกิจยุคดิจิทัล |The Ultimate Business Concepts in Digital Era </b></h5>
+          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>      
+          <hr>
+        </div>
+        
+         <div class="w3-container">
+          <h5 class="w3-opacity"><b> การสร้างสรรค์เนื้อหาด้วยข้อมูล (Data) เพื่อเพิ่มคุณค่าขององค์กร | Content Creation in Digital Organization </b></h5>
+          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>      
+          <hr>
+        </div>
+        <img src="../Pictures/Capture.PNG" style="width:95%">
+        <img src="../Pictures/222.PNG" style="width:95%">
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-blue w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="http://mbs.msu.ac.th/2022/" target="_blank">MBS,MSU</a></p>
+</footer>
+
 </body>
 </html>
