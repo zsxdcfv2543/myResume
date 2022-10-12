2ecd61b552861c6b0d9ca9e99dcbabd9e8efe3fb
zsxdcfv2543 <99699573+zsxdcfv2543@users.noreply.github.com>
2022-10-12 11:01:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>PHP - Hello, World!</title>
 </head>
 <body>
-    <img src="man.jpg" style="width:100%;">
+    <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/242877860_1576174672759179_7529231060200336129_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEhc3VpV1reuGcxrpUeBQEXafgQ-qeS3AJp-BD6p5LcArY90t2a5_y8kAm2Q_MS3x4XvhvQmm4R5Tx5uSZdya4K&_nc_ohc=GRfHFUM_3IUAX_kr8po&_nc_ht=scontent.fkkc3-1.fna&oh=00_AT_uchgRjG8E0AA261F01ylW3GJ0Gr-nBgRzki8PZMQkHw&oe=634B24A1" style="width:100%;">
         <h1><?php echo 'Vachira Poomsaka'; ?></h1>
 </body>
 </html>
